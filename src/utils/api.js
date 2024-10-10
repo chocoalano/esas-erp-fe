cb6eb153be1f0c2da686541e357b2f16a3031cc8
chocoalano <alangentina95@gmail.com>
2024-10-10 15:09:47 +0700

    fgdhgjgftghj

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,7 @@
 // src/utils/api.js
 export function baseApiUrl() {
-  return 'http://172.16.0.117:3333';
+  return 'https://api.sinergiabadisentosa.com'; //for production
+  // return 'http://172.16.0.117:3333'; //for development
 }
 
 export function baseFileUrl() {
